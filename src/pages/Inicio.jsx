992e4e6b9d95cad177716c380c8b56d4a14c90a9
xralks <ramiroandres.sc@gmail.com>
2025-11-13 22:06:34 -0300

    feat: Cree y agregue el componente HeroHome a la page inicio

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,17 +1,9 @@
-import '../styles/Inicio.css'
+import HeroHome from "../components/HeroHome/HeroHome";
 
 const Inicio = () => {
   return (
     <div>
-      <section className="Hero-section">
-        <div className="Hero-cont-texto">
-          <h1>FinanzasIA</h1>
-          <p>Tu Asistente de Finanzas Personales Inteligente</p>
-          <div className="container-botones-hero">
-            <a href="/cursos" className="boton-primario boton-largo">Comenzar ahora</a>
-          </div>
-        </div>
-      </section>
+      <HeroHome />
     </div>
   );
 };
