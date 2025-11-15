d992bf6c4f46f65787b3499225f9a0ae314559cd
xralks <ramiroandres.sc@gmail.com>
2025-11-15 13:16:32 -0300

    Feat: Creación de footerHome

---
+++ src/components/FooterHome/FooterHome.jsx
@@ -0,0 +1,12 @@
+import "./FooterHome.css";
+
+const FooterHome = () => {
+    return (
+        <section className="footerHome">
+            <h1><span className="resaltarTexto"> Da el primer paso hacia tus metas financieras.</span></h1>
+            <p>Únete a Finanzas IA hoy y transforma tu gestión financiera con la potencia de la inteligencia artificial.</p>
+            <a href="/cursos" className="boton-primario">Crear cuenta gratuita</a>
+        </section>
+    );
+}
+export default FooterHome;
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,7 @@
 import HeroHome from "../components/HeroHome/HeroHome";
 import BeneficiosHome from "../components/BeneficiosHome/BeneficiosHome";
 import BentosHome from "../components/BentosHome/BentosHome";
+import FooterHome from "../components/FooterHome/FooterHome";
 
 const Inicio = () => {
   return (
@@ -8,6 +9,7 @@ const Inicio = () => {
       <HeroHome />
       <BeneficiosHome />
       <BentosHome />
+      <FooterHome />
     </div>
   );
 };
