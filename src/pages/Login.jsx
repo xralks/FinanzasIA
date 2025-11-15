08dccd9a1dee8f0cbc030a6f1c7a0e13d696fbf3
xralks <ramiroandres.sc@gmail.com>
2025-11-15 18:16:00 -0300

    feat: se agrega el frontend de login falta conectividad con el backend

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import LoginView from "../components/componentsLogin/LoginView";
+
+const Login = () => {
+  return (
+    <div>
+        <LoginView />
+    </div>
+  );
+};
+export default Login;
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Inicio from '../pages/Inicio'
+import Login from '../pages/Login'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
+      <Route path="/Inicio-Sesion" element={<Login />} />
     </Routes>
   )
 }
