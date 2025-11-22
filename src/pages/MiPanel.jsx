c1b1e9991e6969f4e8b4ac968c9eddaa49ce7370
xralks <ramiroandres.sc@gmail.com>
2025-11-21 23:20:26 -0300

    feat: primer diseño del panel de control general (Hacer muchos cambios al diseño además de funcionalidad con el usuario)

--- src/pages/MiPanel.jsx
+++ src/pages/MiPanel.jsx
@@ -1,10 +1,11 @@
 import NavbarUsuarios from "../components/componentsUsuarios/NavbarUsuarios/NavbarUsuarios";
+import MiPanelBento from "../components/componentsUsuarios/tarjetaBento/MipnelBento";
 
 const MiPanel = () => {
   return (
     <div>
         <NavbarUsuarios />
-        <h1>hola</h1>
+        <MiPanelBento />
     </div>
     );
 };
