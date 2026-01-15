c5b2a6158d7ebc3d5b03b189035128fe29853842
xralks <ramiroandres.sc@gmail.com>
2026-01-15 18:24:08 -0300

    Update: Se eliminina la clase botonlargo ya que afecta a otros componentes y no portaba al nuevo estilo

--- src/components/componentsHome/HeroHome/HeroHome.jsx
+++ src/components/componentsHome/HeroHome/HeroHome.jsx
@@ -9,8 +9,8 @@ const HeroHome = () => {
                     <h1>Toma el control de tus Finanzas</h1>
                     <p>La forma más simple de gestionar tu dinero. Ahorra, presupuesta y alcanza tus metas financieras sin complicaciones.</p>
                     <div className="containerBotonesHero">
-                        <a href="/Inicio-Sesion" className="boton-primario boton-largo">Comenzar ahora</a>
-                        <a href="/Historia" className="boton-secundario boton-largo">Saber Más</a>
+                        <a href="/Inicio-Sesion" className="boton-primario">Comenzar ahora</a>
+                        <a href="/Historia" className="boton-secundario">Saber Más</a>
                     </div>
                 </div>
                 <div className="HeroContImg">
