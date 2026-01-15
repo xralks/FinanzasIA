28271075af43878f987304a86ddf7b18af95da35
xralks <ramiroandres.sc@gmail.com>
2026-01-14 23:36:39 -0300

    Update: Se agrega el navbaar y footer a la pagina historia

--- src/pages/HistoriaApp.jsx
+++ src/pages/HistoriaApp.jsx
@@ -1,9 +1,13 @@
+import NavbarHome from "../components/componentsHome/NavbarHome/NavbarHome";
 import HeroHistoria from "../components/componentsHistoria/HeroHistoria/HeroHistoria";
+import Footer from "../components/Footer/Footer";
 
 const HistoriaApp =() => {
     return (
         <div>
+            <NavbarHome/>
             <HeroHistoria />
+            <Footer/>
         </div>
     );
 };
